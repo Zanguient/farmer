316b97b2515adda39492a52dfc8d8154b6ab317a
Miquéias Oliveira <miqueias@Miqueiass-MacBook-Air.local>
2018-06-10 08:56:44 -0300

    Ajustando porta

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 app.set('port', process.env.PORT || 8080);
+
 app.set('views', __dirname + '/views');
 //app.set('view engine', 'ejs');
 //app.engine('html', require('ejs').renderFile);
